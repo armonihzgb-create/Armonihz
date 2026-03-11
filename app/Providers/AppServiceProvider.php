a1b1907c1559fdbebc0bfdb414218d2c5631b4a1
Zaith232 <b17zaithlc@gmail.com>
2026-03-11 01:22:31 -0600

    se actualizo providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if (config('app.env') === 'production') {
-            Request::setTrustedProxies(['*'], Request::HEADER_X_FORWARDED_ALL);
+            Request::setTrustedProxies(['*'], Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_HOST | Request::HEADER_X_FORWARDED_PORT | Request::HEADER_X_FORWARDED_PROTO | Request::HEADER_X_FORWARDED_AWS_ELB);
             \Illuminate\Support\Facades\URL::forceScheme('https');
         }
         RateLimiter::for ('login', function (Request $request) {
