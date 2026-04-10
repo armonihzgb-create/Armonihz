07d6b2611a86feda59159b5f9826f5b455248a14
Zaith232 <b17zaithlc@gmail.com>
2026-04-10 13:33:54 -0600

    Correcciones

--- app/Http/Controllers/Web/PromotionController.php
+++ app/Http/Controllers/Web/PromotionController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Web;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Promotion;
+use Illuminate\Support\Facades\Auth;
 
 class PromotionController extends Controller
 {
